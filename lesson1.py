6114100ab8954e415a74334fa3d46a85344538cb
orange <skyyi.0227@gmail.com>
2018-06-19 21:36:02 +0800

    hw1

---
+++ lesson1.py
@@ -0,0 +1,17 @@
+n1=input("Enter a Number:")
+n2=input("Enter a Number:")
+n3=input("Enter a Number:")
+n4=input("Enter a Number:")
+n5=input("Enter a Number:")
+n1=int(n1) #int(資料)將資料轉換成整數的型式
+n2=int(n2)
+n3=int(n3)
+n4=int(n4)
+n5=int(n5)
+result=n1+n2+n3+n4+n5
+print(result)
+list=[n1,n2,n3,n4,n5]
+resuit=max(list)
+print(resuit)
+
+
